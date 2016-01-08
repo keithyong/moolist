310c136951f86844553d966387ece84f5b36dc3d
Keith Yong <terda12@gmail.com>
2016-01-08 11:34:06 -0500

    POST /check/:id implemented

--- server.js
+++ server.js
@@ -26,5 +26,23 @@ app.get('/todo', (req, res, next) => {
     });
 });
 
+app.post('/check/:id', (req, res) => {
+    pg.connect(config.pg_connection_string, (err, client, done) => {
+        if (err) {
+            console.error(err);
+            pg_err_handler(res, err);
+        } else {
+            client.query('UPDATE todo SET completed = NOT completed WHERE todo.id=$1', [req.params.id], (err, result) => {
+                if (err) {
+                    pg_err_handler(res, err);
+                } else {
+                    res.status(200);
+                    res.send('Successfully checked/unchecked todo item.');
+                }
+            });
+        }
+    });
+});
+
 app.listen(config.port);
 console.log('Listenining on port ' + config.port);
