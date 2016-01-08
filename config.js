251d301831daedf58d258d6b7ace514fb8341f6f
Keith Yong <terda12@gmail.com>
2016-01-08 11:27:00 -0500

    Added GET /todo route

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    pg_connection_string: "postgres://postgres:postgres@localhost:5432/todo",
+    port: "8085"
+}
--- server.js
+++ server.js
@@ -1,1 +1,30 @@
 var express = require('express');
+var config = require('./config');
+var pg = require('pg');
+var app = express();
+
+var pg_err_handler = (res, err) => {
+    res.status(500);
+    res.send(err);
+};
+
+app.get('/todo', (req, res, next) => {
+    pg.connect(config.pg_connection_string, (err, client, done) => {
+        if (err) {
+            console.error(err);
+            pg_err_handler(res, err);
+        } else {
+            client.query('SELECT * FROM todo', (err, result) => {
+                if (err) {
+                    pg_err_handler(res, err);
+                } else {
+                    res.status(200);
+                    res.send(result.rows);
+                }
+            });
+        }
+    });
+});
+
+app.listen(config.port);
+console.log('Listenining on port ' + config.port);
