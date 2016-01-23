fdc11a857c80f03b523af1dda44d53e71df9077d
Keith Yong <terda12@gmail.com>
2016-01-23 14:39:19 -0500

    Give new todo items maxId + 1 on adding to the state

--- reducers/todo.js
+++ reducers/todo.js
@@ -2,9 +2,15 @@ export default function todosReducer(state = [], action) {
     switch (action.type) {
         case 'ADD_TODO':
             fetch('./todo/' + action.text, {method: 'post'})
+            
+            // Get the maximum ID from the list of todo's
+            const todoWithMaxId = state.reduce((max, todo) => { 
+                return todo.id > max.id ? todo : max 
+            })
+
             return [
                 {
-                    id: action.id,
+                    id: todoWithMaxId.id + 1,
                     completed: false,
                     text: action.text
                 },
